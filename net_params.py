f70bef4c63a9cc7d224ac083151f24287d57f425
lihaobo <2986080061@qq.com>
2022-06-14 20:04:57 +0800

    chang to 4 layer

--- net_params.py
+++ net_params.py
@@ -40,7 +40,7 @@ convgru_encoder_params = [
         OrderedDict({'conv1_leaky_1': [1, 16, 3, 1, 1]}),
         OrderedDict({'conv2_leaky_1': [64, 64, 3, 2, 1]}),
         OrderedDict({'conv3_leaky_1': [96, 96, 3, 2, 1]}),
-        OrderedDict({'conv4_leaky_1': [96, 96, 3, 2, 1]}),
+        OrderedDict({'conv4_leaky_1': [128, 128, 3, 2, 1]}),
     ],
 
     [
@@ -51,13 +51,14 @@ convgru_encoder_params = [
     ]
 ]
 
+
 convgru_decoder_params = [
     [
         OrderedDict({'deconv1_leaky_1': [128, 128, 4, 2, 1]}),
         OrderedDict({'deconv2_leaky_1': [128, 128, 4, 2, 1]}),
-        OrderedDict({'deconv3_leaky_1': [128, 128, 4, 2, 1]}),
+        OrderedDict({'deconv3_leaky_1': [96, 96, 4, 2, 1]}),
         OrderedDict({
-            'conv4_leaky_1': [96, 32, 3, 1, 1],
+            'conv4_leaky_1': [64, 32, 3, 1, 1],
             'conv5_leaky_1': [32, 1, 1, 1, 0]
         }),
     ],
@@ -65,7 +66,7 @@ convgru_decoder_params = [
     [
         CGRU_cell(shape=(15, 19), input_channels=128, filter_size=5, num_features=128),
         CGRU_cell(shape=(30, 38), input_channels=128, filter_size=5, num_features=128),
-        CGRU_cell(shape=(60, 76), input_channels=128, filter_size=5, num_features=128),
-        CGRU_cell(shape=(120, 152), input_channels=128, filter_size=5, num_features=96),
+        CGRU_cell(shape=(60, 76), input_channels=128, filter_size=5, num_features=96),
+        CGRU_cell(shape=(120, 152), input_channels=96, filter_size=5, num_features=64),
     ]
 ]
