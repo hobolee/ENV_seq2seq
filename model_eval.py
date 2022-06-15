136a156040c93816fe9004f64bed9e009d126a68
lihaobo <2986080061@qq.com>
2022-06-15 20:03:44 +0800

    eval, change to cpu, mps will lead to error increasing

--- model_eval.py
+++ model_eval.py
@@ -56,23 +56,23 @@ def plot(pred, label, lon, lat, i, mode):
     ax1 = plt.axes([0.03, 0.1, 0.455, 0.8], projection=ccrs.PlateCarree())
     pred[pred > 150] = 150
     label[label > 150] = 150
-    cf1 = plt.contourf(lon, lat, pred, transform=ccrs.PlateCarree(), levels=range(151))
-    # cf1 = plt.contourf(lon, lat, pred, transform=ccrs.PlateCarree())
+    # cf1 = plt.contourf(lon, lat, pred, transform=ccrs.PlateCarree(), levels=range(151))
+    cf1 = plt.contourf(lon, lat, pred, 60, transform=ccrs.PlateCarree())
     ax1.coastlines()
     ax1.set_title('prediction')
     ax1.set_xlabel('lon')
     ax1.set_ylabel('lat')
     ax2 = plt.axes([0.46, 0.1, 0.455, 0.8], projection=ccrs.PlateCarree())
-    cf2 = plt.contourf(lon, lat, label, transform=ccrs.PlateCarree(), levels=range(151))
-    # cf2 = plt.contourf(lon, lat, label, transform=ccrs.PlateCarree())
+    # cf2 = plt.contourf(lon, lat, label, transform=ccrs.PlateCarree(), levels=range(151))
+    cf2 = plt.contourf(lon, lat, label, 60, transform=ccrs.PlateCarree())
     ax2.set_xlabel('lon')
     ax2.set_title('label')
     ax2.coastlines()
     # plt.subplots_adjust(bottom=0.1, right=0.9, top=0.9)
     cax = plt.axes([0.92, 0.1, 0.025, 0.8])
-    cbar = fig.colorbar(cf2, ax=[ax1, ax2], shrink=1, cax=cax, ticks=[0, 30, 60, 90, 120, 150])
-    cbar.set_ticklabels(['0', '30', '60', '90', '120', '>150'])
-    # cbar = fig.colorbar(cf2, ax=[ax1, ax2], shrink=1, cax=cax)
+    # cbar = fig.colorbar(cf2, ax=[ax1, ax2], shrink=1, cax=cax, ticks=[0, 30, 60, 90, 120, 150])
+    # cbar.set_ticklabels(['0', '30', '60', '90', '120', '>150'])
+    cbar = fig.colorbar(cf2, ax=[ax1, ax2], shrink=1, cax=cax)
     cbar.ax.tick_params(labelsize=12)
     cbar.set_label('No2(ppb)')
     if mode == 'show':
@@ -116,7 +116,16 @@ def eval():
         eval the model
         :return: save the pred_list, label_list, train_loss, valid_loss
         '''
-    TIMESTAMP = "2022-06-11T00-00-00_72to1"
+    random_seed = 1996
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+    if torch.cuda.device_count() > 1:
+        torch.cuda.manual_seed_all(random_seed)
+    else:
+        torch.cuda.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    TIMESTAMP = "2022-06-14T00-00-00_72to10"
     save_dir = './save_model/' + TIMESTAMP
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size',
@@ -144,7 +153,7 @@ def eval():
     encoder = Encoder(encoder_params[0], encoder_params[1])
     decoder = Decoder(decoder_params[0], decoder_params[1])
     net = ED(encoder, decoder)
-    device = torch.device("mps")
+    device = torch.device("cpu")
 
     print('==> loading existing model')
     model_info = torch.load(os.path.join(save_dir, 'checkpoint.pth.tar'), map_location=torch.device('cpu'))
@@ -171,7 +180,8 @@ def eval():
             # input_decoder = inputs.squeeze(dim=2)
             input_decoder = None
             pred = net(inputs, input_decoder)[:, -1, :, :].squeeze()  # B,S,C,H,W
-            # if i == 0:
+            if i == 0:
+                print(pred)
             #     tb.add_graph(net, inputs)
             loss = lossfunction(pred, label)
             loss_aver = loss.item()
@@ -190,7 +200,7 @@ def eval():
     tb.flush()
     tb.close()
     res = [pred_list[:, :, 1:], label_list[:, :, 1:]]
-    np.save('eval_result_diff_72to1', res)
+    np.save('eval_result_diff_72to10', res)
 
 
 def eval_plot():
@@ -199,7 +209,7 @@ def eval_plot():
     plot the loss curve
     :return:
     '''
-    result = np.load('eval_result_diff_72to1.npy', allow_pickle=True)
+    result = np.load('eval_result_diff_72to10.npy', allow_pickle=True)
     pred_list = result[0]
     label_list = result[1]
     aqms_data = torch.load('/Users/lihaobo/PycharmProjects/data_no2/aqms_after_IDW.pt')
@@ -213,7 +223,7 @@ def eval_plot():
     mse_before, mse_after, mse_before_n, mse_after_n, mse_before_m, mse_after_m = [], [], [], [], [], []
     for i in range(1000):
         aqms = aqms_data[::2, ::2, i + 72 + 23]
-        pred = pred_list[:, :, i]*2
+        pred = pred_list[:, :, i]
         label = label_list[:, :, i]
 
         pred, label = diff2adms(pred, label, aqms)
@@ -224,18 +234,18 @@ def eval_plot():
         # mse_before_m.append(mse_b_m)
         # mse_after_m.append(mse_a_m)
 
-        mse_b = cal_mse(pred, label)
-        pred = aqms_correction(pred, weight, i)
-        mse_a = cal_mse(pred, label)
-        mse_before.append(mse_b)
-        mse_after.append(mse_a)
-        mse_b_n = cal_mse(pred, label)
-        pred = negetive_correction(pred)
-        mse_a_n = cal_mse(pred, label)
-        mse_before_n.append(mse_b_n)
-        mse_after_n.append(mse_a_n)
-        cor = cal_cor(pred, label)
-        cor_list.append(cor)
+        # mse_b = cal_mse(pred, label)
+        # pred = aqms_correction(pred, weight, i)
+        # mse_a = cal_mse(pred, label)
+        # mse_before.append(mse_b)
+        # mse_after.append(mse_a)
+        # mse_b_n = cal_mse(pred, label)
+        # pred = negetive_correction(pred)
+        # mse_a_n = cal_mse(pred, label)
+        # mse_before_n.append(mse_b_n)
+        # mse_after_n.append(mse_a_n)
+        # cor = cal_cor(pred, label)
+        # cor_list.append(cor)
 
         plot(pred, label, lon, lat, i, 'save')
     print(np.mean(cor_list))
@@ -248,7 +258,7 @@ def eval_plot():
 
 
 def eval_ts():
-    result = np.load('eval_result_diff_72to1.npy', allow_pickle=True)
+    result = np.load('eval_result_diff_72to10.npy', allow_pickle=True)
     pred_list = result[0]
     label_list = result[1]
     aqms_data = torch.load('/Users/lihaobo/PycharmProjects/data_no2/aqms_after_IDW.pt')
@@ -257,12 +267,12 @@ def eval_ts():
     weight = weight.reshape([-1, 14])
     cor_list, pred_station, label_station = [], [], []
     # station = [78, 182]
-    station = [83, 60]
+    station = [78, 182]
     for i in range(1000):
         aqms = aqms_data[::2, ::2, i + 72 + 23]
         pred = pred_list[:, :, i]
         label = label_list[:, :, i]
-        # pred, label = diff2adms(pred, label, aqms)
+        pred, label = diff2adms(pred, label, aqms)
         # pred = mean_corection(pred, aqms)
         # pred = negetive_correction(pred)
         # pred = aqms_correction(pred, weight, i)
